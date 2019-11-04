e261e8babaec78665512fd21cf0c48e6a6ae821d
martinyounghoonkim <hoons0131@gmail.com>
2019-11-04 23:45:09 +0900

    Add sample code to creatPage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,18 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const pages = [
+  { id: 1, content: 'Gatsby 로 블로그 만들기' },
+  { id: 2, content: '거기에 타입스크립트 적용 해 보기' },
+  { id: 3, content: '확실히 어렵네요' },
+];
 
-// You can delete this file if you're not using it
+exports.createPages = ({ actions }) => {
+  const { createPage } = actions;
+  const templateComponent = path.resolve('src/pages/post-templates.js');
+  pages.forEach(page => {
+    createPage({
+      path: page.id.toString(),
+      context: "page",
+      component: templateComponent
+    })
+  })
+};
